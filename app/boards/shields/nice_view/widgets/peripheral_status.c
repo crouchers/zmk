61426cc2c7d6ddc4569f8aa9e6c3e637dc6d5d37
A Crouch <aaron_crouch_2003@yahoo.com>
2025-03-02 16:30:00 -0500

    Update peripheral_status.c

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -23,8 +23,9 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include "peripheral_status.h"
 
-LV_IMG_DECLARE(balloon);
-LV_IMG_DECLARE(mountain);
+# LV_IMG_DECLARE(balloon);
+# LV_IMG_DECLARE(mountain);
+LV_IMG_DECLARE(bw_test);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -116,7 +117,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, random ? &balloon : &mountain);
+    # lv_img_set_src(art, random ? &balloon : &mountain);
+    lv_img_set_src(art, &bw_test);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
