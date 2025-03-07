30291d8e6eae65d91b093f13e63af59cea9314a3
A Crouch <aaron_crouch_2003@yahoo.com>
2025-03-07 15:12:48 -0500

    sun_moon

--- app/boards/shields/nice_view/widgets/peripheral_status.c
+++ app/boards/shields/nice_view/widgets/peripheral_status.c
@@ -25,7 +25,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 // LV_IMG_DECLARE(balloon);
 // LV_IMG_DECLARE(mountain);
-LV_IMG_DECLARE(test_inv);
+LV_IMG_DECLARE(sun_moon);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -117,7 +117,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     bool random = sys_rand32_get() & 1;
-    lv_img_set_src(art, &test_inv);
+    lv_img_set_src(art, &sun_moon);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
